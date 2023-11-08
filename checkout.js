c378cf007922da228dd8c44d8139145c3e0cb378
sahlanissar <sahlanissar123@gmail.com>
2023-11-08 22:20:38 +0400

    checkout with registration

--- checkout.js
+++ checkout.js
@@ -1,5 +1,4 @@
 const { Builder, By, Key, until, Browser } = require('selenium-webdriver');
-const assert = require('assert');
 require("chromedriver");
 const chrome = require('selenium-webdriver/chrome');
 
@@ -12,65 +11,31 @@ const driver = new Builder().forBrowser(Browser.CHROME).setChromeOptions(options
 // Website URLs
 const baseUrl = 'https://www.mumzworld.com/en/';
 
-// User credentials
-const username = 'sahlanissar123@gmail.com';
-const password = 'Sahlanissar@123';
-
 // Start the test
 (async function () {
   try {
+    const shortTimeOut = 10000
+    const mediumTimeOut = 25000
     // Full screeen
     options.addArguments('--start-maximized');
     // Open the main page
     await driver.get(baseUrl);
-
-    const loginButton = await driver.findElement(By.css(`[title="Login"]`));
     let cartUrl = `${baseUrl}cart`
 
-    // Click on the login button
-    await loginButton.click();
-
-    // // Fill in the login form and submit
-    await driver.findElement(By.name('email')).sendKeys(username);
-    await driver.findElement(By.name('password')).sendKeys(password);
-    await driver.findElement(By.css(`[title="Sign in"]`)).click();
-
-    // // Wait for login to complete
-    const userClassName = "accountTrigger-triggerText-1Mu"
-    await driver.wait(until.elementTextIs(driver.findElement(By.className(userClassName)), 'Hi, sahla'), 10000);
-
-
-    // CLEAR CART
-    await driver.get(cartUrl);
-    await driver.wait(until.urlIs(cartUrl), 10000);
-
-    await driver.wait(until.elementLocated(By.className('cartPage-heading-3f_')), 10000);
-    await driver.wait(until.elementTextIs(driver.findElement(By.className('cartPage-heading-3f_')), 'Shopping bag'), 10000);
-
-    // Locate all "Remove" buttons for items in the cart.
-    const removeButtons = await driver.findElements(By.className('product-removeItem-2gC'));
-
-    // Click each "Remove" button to clear the cart.
-    for (const button of removeButtons){
-      await button.click();
-    }
-
-    // CLEAR CART
     await driver.get(baseUrl);
-    await driver.wait(until.urlIs(baseUrl), 10000);
+    await driver.wait(until.urlIs(baseUrl), shortTimeOut);
 
     let productName = "Simba Minnie Spring Palms Trolley"
-    // search item 
+    // search item
     await driver.findElement(By.css(`[type="search"]`)).sendKeys(productName)
     await driver.findElement(By.css(`[title="Submit your search query"]`)).click();
 
-
     // Expected search URL
     let searchUrl = baseUrl + "search.html?q=" + encodeURIComponent(productName)
-    await driver.wait(until.urlIs(searchUrl), 10000);
+    await driver.wait(until.urlIs(searchUrl), shortTimeOut);
 
     // Add to cart button
-    await driver.wait(until.elementLocated(By.className('galleryCartButton-cartBuyIcon-3LD')), 10000);
+    await driver.wait(until.elementLocated(By.className('galleryCartButton-cartBuyIcon-3LD')), shortTimeOut);
     await driver.findElement(By.tagName('body')).click();
     // click on bag button
     await driver.findElement(By.css(`[aria-label="Add to Cart"]`)).click();
@@ -79,40 +44,56 @@ const password = 'Sahlanissar@123';
     let successCartClass = "feedbackPopup-success-2gz"
     let successCartMessage = "Successfully added to bag"
     let addTocartSuccessElement = By.className(successCartClass)
-    await driver.wait(until.elementLocated(addTocartSuccessElement), 10000);
-    await driver.wait(until.elementTextIs(driver.findElement(addTocartSuccessElement), successCartMessage), 10000);
-
+    await driver.wait(until.elementLocated(addTocartSuccessElement), shortTimeOut);
+    await driver.wait(until.elementTextIs(driver.findElement(addTocartSuccessElement), successCartMessage), shortTimeOut);
 
     // click on view bag button
     let viewBag = By.css(`[title="View Bag"]`)
     await driver.findElement(viewBag).click()
 
     // Wait for cart page loading
-    await driver.wait(until.urlIs(cartUrl), 20000);
+    await driver.wait(until.urlIs(cartUrl), shortTimeOut);
 
+    await driver.wait(until.elementLocated(By.className("orderSummary-stickySection-2NR")), 20000);
 
     // Increase qty to be 5 items
 
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i < 5; i++) {
         const increaseButton = By.xpath(`//*[@id="root"]/main/div[2]/div/div[2]/div[1]/div[1]/div/ul/li/div/div[2]/div/form/div/button[2]`)
         const quantityInput = By.name(`quantity`)
-        await driver.wait(until.elementLocated(increaseButton), 10000);
+        await driver.wait(until.elementLocated(increaseButton), shortTimeOut);
         const increaseButtonElement = await driver.findElement(increaseButton);
-        // await driver.wait(until.elementLocated(quantityInput), 10000);
+        await driver.wait(until.elementLocated(quantityInput), shortTimeOut);
         await increaseButtonElement.click();
-        console.log(i)
     }
 
+    let itemCount = By.className('cartItemsQty-root-1IW')
+    await driver.wait(until.elementLocated(itemCount), shortTimeOut);
+    await driver.wait(until.elementTextIs(driver.findElement(itemCount), '5 items'), shortTimeOut);
+    await driver.wait(until.elementLocated(By.className('cartPage-summary_container-3AF')), shortTimeOut);
+    await driver.findElement(By.tagName('body')).click();
+ 
     // Click Proceed to checkout
-    // title="Proceed to Checkout"
-    let checkout = By.css(`[title="Proceed to Checkout"]`)
+    let checkout = By.className("proceedToCheckoutBtn-button-3S2")
+    await driver.wait(until.elementLocated(checkout), mediumTimeOut);
     await driver.findElement(checkout).click()
-    
+
+    const signInUrl = `${baseUrl}sign-in`
+    await driver.wait(until.urlIs(signInUrl), 20000);
+
+    // Sign Up
+    const signUp = By.className(`signLink-link-2ye`)
+    await driver.wait(until.elementLocated(signUp), shortTimeOut);
+    await driver.findElement(signUp).click()
+    const signUpUrl = `${baseUrl}create-account`
+    await driver.wait(until.urlIs(signUpUrl), mediumTimeOut);
 
     console.log('Test Passed: Product added to the wishlist successfully.');
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Test Failed: ' + error.message);
-  } finally {
+  }
+  finally {
     // Close the browser
     await driver.quit();
   }
